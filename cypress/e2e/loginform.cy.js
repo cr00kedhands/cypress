fdb898a9115fe24b54222361fdf3dba91407fc01
Irina <tkachenko.irina92@gmail.com>
2024-09-02 18:21:48 +0300

    spo

--- cypress/e2e/loginform.cy.js
+++ cypress/e2e/loginform.cy.js
@@ -1,55 +1,57 @@
+import * as data from "../data/logpass.json"
+import * as main_page from "../locators/main_page.json"
+import * as recovery_pass_page from "../locators/recovery_pass_page.json"
+import * as result_page from "../locators/result_page.json"
+
 describe('Проверка авторизации', function () {
 
-     it('Верный логин и верный пароль', function () {
-        cy.visit('/'); // открыть сайт
-        cy.get('#mail').type('german@dolnikov.ru'); //выбрать инпут логин и ввести верный логин
-        cy.get('#pass').type('iLoveqastudio1'); //выбрать инпут пароль и ввести верный пароль
-        cy.get('#loginButton').click(); //выбрать кнопку войти и нажать на нее
-        cy.get('#messageHeader').contains('Авторизация прошла успешно'); //проверить успешность авторизации
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //видно крестик выхода
-        })
+  beforeEach('Начало теста', function () {
+    cy.visit('/'); //открыть сайт
+  })
 
-     it('Проверка логики восстановления пароля', function () {
-        cy.visit('https://login.qa.studio//'); // открыть сайт
-        cy.get('#forgotEmailButton').click(); //выбрать кнопку «Забыли пароль» и нажать на нее
-        cy.get('#mailForgot').type('german@dolnikov.ru'); //Ввести любой имейл
-        cy.get('#restoreEmailButton').click();//Нажать на кнопку "Отправить код"
-        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail'); //проверить нужное сообщения
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //видно крестик выхода
-        })
+  afterEach('Конец теста', function () {
+    cy.get(result_page.exit).should('be.visible'); //видно крестик выхода
+  })
+  
+  it('Верный логин и верный пароль', function () {
+    cy.get(main_page.login).type(data.login); //выбрать инпут логин и ввести верный логин
+    cy.get(main_page.pass).type(data.password); //выбрать инпут пароль и ввести верный пароль
+    cy.get(main_page.enter).click(); //выбрать кнопку войти и нажать на нее
+    cy.get(result_page.message).contains('Авторизация прошла успешно'); //проверить успешность авторизации
+  })
 
-      it('Верный логин и НЕверный пароль', function () {
-        cy.visit('https://login.qa.studio//'); // открыть сайт
-        cy.get('#mail').type('german@dolnikov.ru'); //выбрать инпут логин и ввести верный логин
-        cy.get('#pass').type('iLoveqastudio9'); //выбрать инпут пароль и ввести НЕверный пароль
-        cy.get('#loginButton').click(); //выбрать кнопку войти и нажать на нее
-        cy.get('#messageHeader').contains('Такого логина или пароля нет'); //проверить нужный текст
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //видно крестик выхода
-        })
+  it('Проверка логики восстановления пароля', function () {
+    cy.get(main_page.forgot).click(); //выбрать кнопку «Забыли пароль» и нажать на нее
+    cy.get(recovery_pass_page.email).type(data.login); //Ввести любой имейл
+    cy.get(recovery_pass_page.button).click();//Нажать на кнопку "Отправить код"
+    cy.get(result_page.message).contains('Успешно отправили пароль на e-mail'); //проверить нужное сообщения
+  })
 
-      it('НЕверный логин и верный пароль', function () {
-        cy.visit('https://login.qa.studio//'); // открыть сайт
-        cy.get('#mail').type('german@dolnikovvvv.ru'); //выбрать инпут логин и ввести НЕверный логин
-        cy.get('#pass').type('iLoveqastudio1'); //выбрать инпут пароль и ввести верный пароль
-        cy.get('#loginButton').click(); //выбрать кнопку войти и нажать на нее
-        cy.get('#messageHeader').contains('Такого логина или пароля нет'); //проверить нужный текст
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //видно крестик выхода
-        })
+  it('Верный логин и НЕверный пароль', function () {
+    cy.get(main_page.login).type(data.login); //выбрать инпут логин и ввести верный логин
+    cy.get(main_page.pass).type('iLoveqastudio9'); //выбрать инпут пароль и ввести НЕверный пароль
+    cy.get(main_page.enter).click(); //выбрать кнопку войти и нажать на нее
+    cy.get(result_page.message).contains('Такого логина или пароля нет'); //проверить нужный текст
+  })
 
-      it('Негативный кейс валидации', function () {
-        cy.visit('https://login.qa.studio//'); // открыть сайт
-        cy.get('#mail').type('germandolnikov.ru'); //выбрать инпут логин и ввести логин без @
-        cy.get('#pass').type('iLoveqastudio1'); //выбрать инпут пароль и ввести верный пароль
-        cy.get('#loginButton').click(); //выбрать кнопку войти и нажать на нее
-        cy.get('#messageHeader').contains('Нужно исправить проблему валидации'); //получаем текст с ошибкой
-        })
+  it('НЕверный логин и верный пароль', function () {
+    cy.get(main_page.login).type('german@dolnikovvvv.ru'); //выбрать инпут логин и ввести НЕверный логин
+    cy.get(main_page.pass).type(data.password); //выбрать инпут пароль и ввести верный пароль
+    cy.get(main_page.enter).click(); //выбрать кнопку войти и нажать на нее
+    cy.get(result_page.message).contains('Такого логина или пароля нет'); //проверить нужный текст
+  })
 
-      it('Проверка на приведение к строчным буквам в логине', function () {
-        cy.visit('https://login.qa.studio//'); // открыть сайт
-        cy.get('#mail').type('GerMan@Dolnikov.ru'); //выбрать инпут логин и ввести верный логин с разным регистром
-        cy.get('#pass').type('iLoveqastudio1'); //выбрать инпут пароль и ввести верный пароль
-        cy.get('#loginButton').click(); //выбрать кнопку войти и нажать на нее
-        cy.get('#messageHeader').contains('Авторизация прошла успешно'); //проверить успешность авторизации
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //видно крестик выхода
-        }) 
+  it('Негативный кейс валидации', function () {
+    cy.get(main_page.login).type('germandolnikov.ru'); //выбрать инпут логин и ввести логин без @
+    cy.get(main_page.pass).type(data.password); //выбрать инпут пароль и ввести верный пароль
+    cy.get(main_page.enter).click(); //выбрать кнопку войти и нажать на нее
+    cy.get(result_page.message).contains('Нужно исправить проблему валидации'); //получаем текст с ошибкой
+  })
+
+  it('Проверка на приведение к строчным буквам в логине', function () {
+    cy.get(main_page.login).type('GerMan@Dolnikov.ru'); //выбрать инпут логин и ввести верный логин с разным регистром
+    cy.get(main_page.pass).type(data.password); //выбрать инпут пароль и ввести верный пароль
+    cy.get(main_page.enter).click(); //выбрать кнопку войти и нажать на нее
+    cy.get(result_page.message).contains('Авторизация прошла успешно'); //проверить успешность авторизации
+  }) 
 })
